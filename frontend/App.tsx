a591521349595d5c81b515d0b725d7c768df06a7
Mit <mit4sheth@gmail.com>
2026-02-17 23:20:10 +0530

    setup redux store & provider

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -3,8 +3,10 @@ import { useEffect } from 'react';
 import { View, StatusBar, AppState, type AppStateStatus } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Provider } from 'react-redux';
 import { AuthProvider } from './src/auth/AuthContext';
 import { MainContainer } from './src/screens/Main';
+import { store } from './src/store';
 import { logApp } from './src/utils/logger';
 
 export default function App() {
@@ -24,15 +26,17 @@ export default function App() {
   }, []);
 
   return (
-    <GestureHandlerRootView className="flex-1">
-      <SafeAreaProvider>
-        <AuthProvider>
-          <View className="flex-1 bg-screen">
-            <MainContainer />
-          </View>
-          <StatusBar barStyle="light-content" />
-        </AuthProvider>
-      </SafeAreaProvider>
-    </GestureHandlerRootView>
+    <Provider store={store}>
+      <GestureHandlerRootView className="flex-1">
+        <SafeAreaProvider>
+          <AuthProvider>
+            <View className="flex-1 bg-screen">
+              <MainContainer />
+            </View>
+            <StatusBar barStyle="light-content" />
+          </AuthProvider>
+        </SafeAreaProvider>
+      </GestureHandlerRootView>
+    </Provider>
   );
 }
